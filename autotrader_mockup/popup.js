e59f74a9d4f183a16220c8f5f3275af8eb850596
forresti <fiandola@gmail.com>
2014-07-20 20:46:10 -0700

    trying and failing to load DOM... argh

--- autotrader_mockup/popup.js
+++ autotrader_mockup/popup.js
@@ -1,6 +1,6 @@
 
 //thanks: http://stackoverflow.com/questions/11493995/display-current-url-in-a-chrome-extension
-
+// parse page title:
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
   function(tabs){
     //alert(tabs[0].url); //this works -- shows extension in a traditional popup.
@@ -9,7 +9,41 @@ chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}
     //var t=document.createTextNode(tabs[0].url);
     var t=document.createTextNode(tabs[0].title);
     h.appendChild(t);
-    document.body.appendChild(h);
+    //document.body.appendChild(h);
   }
 );
+
+
+//DOM experiments -- not working at all right now.
+
+var urlRegex = /^https?:\/\/(?:[^\.]+\.)?stackoverflow\.com/;
+
+//thanks: http://stackoverflow.com/questions/19758028/chrome-extension-get-dom-content
+/* A function creator for callbacks */
+function doStuffWithDOM(domContent) {
+    console.log("I received the following DOM content:\n" + domContent);
+    var h=document.createElement("h4");
+    //var t=document.createTextNode(domContent);
+    var t=document.createTextNode("hello from doStuffWithDOM()");
+    h.appendChild(t);
+    document.body.appendChild(h);
+}
+
+/* When the browser-action button is clicked... */
+//chrome.browserAction.onClicked.addListener(function(tab) {
+document.addEventListener('DOMContentLoaded', function (tab) {
+    /*...check the URL of the active tab against our pattern and... */
+    //if (urlRegex.test(tab.url)) {
+        var h = document.createElement("h4");
+        h.appendChild(document.createTextNode("hello from addListener() callback");
+        document.body.appendChild(h);
+
+        /* ...if it matches, send a message specifying a callback too */
+        chrome.tabs.sendMessage(tab.id, { text: "report_back" },
+                                doStuffWithDOM);
+    //}
+});
+
+
+
 
