10b47e69ec5acd44034893ec7984d9d0a8ea884b
forresti <fiandola@gmail.com>
2014-09-24 13:37:40 -0700

    rethinking this... going into inject into page instead of popup.

--- autotrader_mockup/content.js
+++ autotrader_mockup/content.js
@@ -1,9 +1,19 @@
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing 
-           the web-pages DOM content as argument */
-        sendResponse(document.all[0].outerHTML);
-    }
-});
+var current,
+    interval;
+
+var checker = function(){
+
+    //var menuWrapper = document.getElementById(" ")
+    var priceHeader = document.getElementsByClassName("primary-price");
+    console.log(priceHeader);    
+
+    var h=document.createElement("h4");
+    var t=document.createTextNode("hi from forrest");
+    h.appendChild(t);
+    document.body.appendChild(h);
+}
+
+console.log("hi from forrest");
+//interval = setInterval(checker,1000)
+checker();
+
