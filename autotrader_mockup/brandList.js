0aed1e975432e52fba7744ef350dc21c223d9ed8
forresti <fiandola@gmail.com>
2014-09-24 14:54:11 -0700

    adding list of brands

---
+++ autotrader_mockup/brandList.js
@@ -0,0 +1,3 @@
+
+var brandList = ['Audi', 'Acura']
+
--- autotrader_mockup/content.js
+++ autotrader_mockup/content.js
@@ -1,8 +1,32 @@
 var current,
     interval;
 
-//get car info from autotrader page
-//var getCarInfo = function(){...}
+//getting new_or_used and year is easy... but make and model is more involved:
+var getMakeModel = function(ymm_str){
+
+    makeModel = {"make":"Subaru", "model":"BRZ"};
+    return makeModel;
+}
+
+//get car info from autotrader page ... year, make, model (ymm)
+var ymm_parse = function(){
+
+    var ymm_header = document.getElementsByClassName("listing-title atcui-block");
+    var ymm_str = ymm_header[0].innerHTML;
+    var ymm_parts = ymm_str.split(" ");
+    var new_or_used = ymm_parts[0];
+    var year = ymm_parts[1];
+
+    makeModel = getMakeModel(ymm_str);
+    
+    var ymmData = {"ymm_str":ymm_str};
+    ymmData.new_or_used = new_or_used;
+    ymmData.year = year;
+    ymmData.make = makeModel.make;
+    ymmData.model = makeModel.model;
+
+    return ymmData; //e.g. new_or_used:new, year:2014, make:Subaru, model:BRZ
+}
 
 //get car price from our server (which queries Edmunds)
 //var requestCarPrice = function(){...}
@@ -26,7 +50,10 @@ var injector = function(){
 }
 
 console.log("hi from forrest");
+console.log("brandList: " + brandList); //included from brandList.js (see manifest.json for how this works)
 
+ymm_json = ymm_parse();
+console.log(JSON.stringify(ymm_json));
 
 //interval = setInterval(injector,1000)
 injector();
